069b9c4764ea77b287a73fa9b2687f7481615e11
zkw0816-lgtm <zkw0816@gmail.com>
2026-09-24 15:32:22 +0800

    新增函数用法

--- cpp-practice/46-max-function.cpp
+++ cpp-practice/46-max-function.cpp
@@ -2,12 +2,12 @@
 #include<cstdio>
 #include<iostream>
 using namespace std;
-int Max (int x,int y)
-{
-    if(x>y)
-        return x;
+int Max (int x,int y)//返回值类型 函数名（参数1类型 参数1名称，参数2类型 参数2名称，……）
+{                    //{
+    if(x>y)          //语句组
+        return x;    //}
     return y;
-}
+}                   
 int main()
 {
     int n=Max(4,6);
